7865858f311b2963e9d3941e26189652f7f1b381
Olek <an00010@gmail.com>
2021-04-06 19:22:25 +0200

    Lab 2

---
+++ MobileApps_Lab2/MobileApps_Lab2/MobileApps_Lab2/OldMainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MobileApps_Lab2
+{
+    public partial class OldMainPage : ContentPage
+    {
+        public OldMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
